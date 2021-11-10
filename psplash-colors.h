bf6ab3f08a332206d775ead7d73cbe7c8e9481b4
Tanner Hunsucker <tanner.hunsucker@ultratec.com>
2021-11-10 16:27:48 -0600

    change colors

--- psplash-colors.h
+++ psplash-colors.h
@@ -12,15 +12,15 @@
 #define _HAVE_PSPLASH_COLORS_H
 
 /* This is the overall background color */
-#define PSPLASH_BACKGROUND_COLOR 0xec,0xec,0xe1
+#define PSPLASH_BACKGROUND_COLOR 0x08,0x3b,0x8c
 
 /* This is the color of any text output */
-#define PSPLASH_TEXT_COLOR 0x6d,0x6d,0x70
+#define PSPLASH_TEXT_COLOR 0xff,0xff,0xff
 
 /* This is the color of the progress bar indicator */
 #define PSPLASH_BAR_COLOR 0x6d,0x6d,0x70
 
 /* This is the color of the progress bar background */
-#define PSPLASH_BAR_BACKGROUND_COLOR 0xec,0xec,0xe1
+#define PSPLASH_BAR_BACKGROUND_COLOR 0x08,0x3b,0x8c
 
 #endif
