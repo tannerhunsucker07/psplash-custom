79c46f77d2f7fc3b414d6dcf0738f506975f2608
Tanner Hunsucker <tanner.hunsucker@ultratec.com>
2021-11-11 10:46:41 -0600

    update colors again

--- psplash-colors.h
+++ psplash-colors.h
@@ -12,8 +12,8 @@
 #define _HAVE_PSPLASH_COLORS_H
 
 /* This is the overall background color */
-#define PSPLASH_MESSAGE_BOX_COLOR 0x08,0x39,0x6b
-#define PSPLASH_BOTTOM_BOX_COLOR 0x00,0x4a,0x0d
+#define PSPLASH_MESSAGE_BOX_COLOR 0x00,0x52,0x8c
+#define PSPLASH_BOTTOM_BOX_COLOR 0x00,0x52,0x8c
 
 /* This is the color of any text output */
 #define PSPLASH_TEXT_COLOR 0xff,0xff,0xff
